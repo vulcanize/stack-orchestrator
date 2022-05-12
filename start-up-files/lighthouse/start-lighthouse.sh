00147a86df4fb1faddf7fb57dcd902ad1272d79b
Abdul Rabbani <abdulrabbani00@gmail.com>
2022-05-12 08:11:23 -0400

    Update README and startup

--- start-up-files/lighthouse/start-lighthouse.sh
+++ start-up-files/lighthouse/start-lighthouse.sh
@@ -4,6 +4,6 @@
 # The 0.0.0.0 is not safe.
 
 lighthouse bn  \
-    --http --http-address 0.0.0.0 --metrics --private --reconstruct-historic-states --network $NETWORK &
+    --http --http-address 0.0.0.0 --metrics --private --network $NETWORK &
 
 tail -f /dev/null
