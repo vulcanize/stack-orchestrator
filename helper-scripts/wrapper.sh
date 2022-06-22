6f00758c16404d227e06321b65149fc512ea1997
nikugogoi <nabarun@deepstacksoft.com>
2022-06-22 17:00:29 +0530

    Add flag to auto mine blocks for running erc721-watcher (#34)
    * Add flag to auto mine blocks in geth
    
    * Change block auto mining interval to 2 seconds
    
    * Increase interval for healthcheck

--- helper-scripts/wrapper.sh
+++ helper-scripts/wrapper.sh
@@ -27,6 +27,8 @@ Spin up Foundry with Geth and a database.
 
 -s          If we are building the full stack, specify the DB version youd like: "v3" or "v4".
 
+-m          Should mining of blocks be done in intervals? "true" or "false"
+
 -p,         Path to config.sh file.
 
 EOF
@@ -42,10 +44,11 @@ p="../config.sh"
 f="false"
 l="remote"
 s="v3"
+m="false"
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
-while getopts ":e:d:v:u:n:p:f:l:s:" o; do
+while getopts ":e:d:v:u:n:p:f:l:s:m:" o; do
     case "${o}" in
         e)
             e=${OPTARG}
@@ -79,6 +82,10 @@ while getopts ":e:d:v:u:n:p:f:l:s:" o; do
         l)
             l=${OPTARG}
             [ "$l" = "local" -o "$l" = "latest" ] || showHelp
+            ;;
+        m)
+            m=${OPTARG}
+            [ "$m" = "true" -o "$m" = "false" ] || showHelp
             ;;
         *)
             showHelp
@@ -144,6 +151,10 @@ fi
 
 echo -e "${GREEN} Sourcing: $p ${nc}"
 source $p
+
+if [ "$m" == "true" ]; then
+    export genesis_file_path='start-up-files/go-ethereum/genesis-automine.json'
+fi
 
 fileArgs=()
 for files in "${composeFiles[@]}"; do fileArgs+=(-f "$files"); done
